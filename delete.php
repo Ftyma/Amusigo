e3996b7042b2389b668c0c53377aa8dd97cab618
roselyn-rasleen <roselyn.rasleen@gmail.com>
2023-10-30 15:16:02 +0700

    nothing

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+require_once('connect.php');
+$p_id = $_GET['id'];
+$q="DELETE FROM user_musicbank WHERE Song_ID=$p_id";
+if(!$mysqli->query($q)){
+	echo "DELETE failed. Error: ".$mysqli->error;}
+$mysqli->close();
+?>
