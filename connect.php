51c06e6fcf1a5f468e11afcfbce1fb306c2c7a5a
roselyn-rasleen <roselyn.rasleen@gmail.com>
2023-11-13 18:27:00 +0700

    t

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost','root','root','music');
+$mysqli = new mysqli('localhost:3307','root','root','music');
    if($mysqli->connect_errno){
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
    }
